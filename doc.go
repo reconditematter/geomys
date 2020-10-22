f66d585362b370f5c611016240896ca0dedf7f44
Leonid Kneller <recondite.matter@gmail.com>
2020-10-22 09:32:30 -0400

    Add copyright (c).

--- doc.go
+++ doc.go
@@ -1,2 +1,6 @@
+// Copyright (c) 2019-2020 Leonid Kneller. All rights reserved.
+// Licensed under the MIT license.
+// See the LICENSE file for full license information.
+
 // Package geomys -- provides miscellaneous geographic and geodesic functions.
 package geomys
--- mapproj.go
+++ mapproj.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2019-2020 Leonid Kneller. All rights reserved.
+// Licensed under the MIT license.
+// See the LICENSE file for full license information.
+
 package geomys
 
 // MapProjection -- a method to transfrom a globe's surface into a plane.
